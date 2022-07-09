c5f7925d196bd899e46f08b80c879811b636d868
Georgios Konstantopoulos <me@gakonst.com>
2022-07-08 20:04:11 -0700

    client: query abci

--- evm-abci/src/bin/client.rs
+++ evm-abci/src/bin/client.rs
@@ -1,88 +1,43 @@
-use abci::types::Header;
 use ethers::prelude::*;
+use evm_abci::types::{Query, QueryResponse};
 use eyre::Result;
-
-use tendermint_abci::ClientBuilder;
-use tendermint_proto::abci::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = ClientBuilder::default().connect("127.0.0.1:26658").unwrap();
-    let res = client.info(RequestInfo::default()).unwrap();
-    let height = res.last_block_height + 1;
-    println!("RequestInfo: {:?}", res);
+    // the ABCI port
+    let host = "http://127.0.0.1:3002";
 
-    let mut client = ClientBuilder::default().connect("127.0.0.1:26658").unwrap();
-    match client.init_chain(RequestInitChain::default()) {
-        Ok(res) => println!("{:?}", res),
-        Err(err) => println!("Skipping, already initialized {:?}", err),
-    };
+    let val = ethers::utils::parse_units(1, 18).unwrap();
+    let alice = "0x2c47Faeff6ED706E9C1D95f51F1800938Cf7c632".parse::<Address>()?;
+    let bob = Address::random();
 
-    let mut client = ClientBuilder::default().connect("127.0.0.1:26658").unwrap();
-    let res2 = client
-        .query(RequestQuery {
-            data: "88f9b82462f6c4bf4a0fb15e5c3971559a316e7f".into(),
-            path: "".to_string(),
-            height,
-            prove: false,
-        })
-        .unwrap();
-    let bob_balance = U256::from_little_endian(&res2.value);
-    println!("Balance before {:?}", bob_balance);
+    let _tx = TransactionRequest::new()
+        .from(alice)
+        .to(bob)
+        .value(val)
+        .gas(21000);
 
-    let mut client = ClientBuilder::default().connect("127.0.0.1:26658").unwrap();
-    let begin_block = RequestBeginBlock {
-        header: Some(Header {
-            height,
-            app_hash: res.last_block_app_hash,
-            ..Default::default()
-        }),
-        ..Default::default()
-    };
-    if let Err(err) = client.begin_block(begin_block) {
-        println!("Not in beginblock state {:?}", err);
-    } else {
-        println!("BeginBlock");
-    }
+    let client = reqwest::Client::new();
 
-    client
-        .deliver_tx(RequestDeliverTx {
-            tx: serde_json::to_vec(
-                &TransactionRequest::new()
-                    .from(
-                        "0x8fd379246834eac74b8419ffda202cf8051f7a03"
-                            .parse::<Address>()
-                            .unwrap(),
-                    )
-                    .to("0x88f9b82462f6c4bf4a0fb15e5c3971559a316e7f"
-                        .parse::<Address>()
-                        .unwrap())
-                    .value(500u64)
-                    .gas(40000u64)
-                    .gas_price(875000000u64),
-            )
-            .unwrap(),
-        })
-        .unwrap();
-
-    client.end_block(RequestEndBlock { height })?;
-
-    let mut client = ClientBuilder::default().connect("127.0.0.1:26658").unwrap();
-    client.commit()?;
-
-    let mut client = ClientBuilder::default().connect("127.0.0.1:26658").unwrap();
+    let query = Query::Balance(alice);
+    let query = serde_json::to_vec(&query).unwrap();
     let res = client
-        .query(RequestQuery {
-            data: "88f9b82462f6c4bf4a0fb15e5c3971559a316e7f".into(),
-            path: "".to_string(),
-            height,
-            prove: false,
-        })
-        .unwrap();
-    let bob_balance = U256::from_little_endian(&res.value);
-    println!("Balance after {:?}", bob_balance);
+        .get(format!("{}/abci_query", host))
+        .query(&[("data", hex::encode(query)), ("path", "".to_string())])
+        .send()
+        .await?;
+    let val = res.bytes().await?;
+    dbg!(&val);
+    let val: QueryResponse = serde_json::from_slice(&val)?;
+    let balance = val.as_balance();
 
-    println!("END");
+    println!("Alice balance before: {}", balance);
+
+    // Query balance before
+
+    // Execute transaction
+
+    // Query balance after
 
     Ok(())
 }
